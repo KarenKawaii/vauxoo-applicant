72b9801b516413a785b47c4de7dfce7e8a26c1a4
KarenKawaii <karenamaroc@gmail.com>
2015-05-07 15:24:37 -0500

    Agrega nuevo script employee_vauxoo.sql

--- calculator.py
+++ calculator.py
@@ -1,16 +1,30 @@
 """
-Your module documentation here
+This module contains a class and functions to make an addition.
 """
+#It declares the class
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    This class make a calculation. Contains the sum method.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Recieves as parameters the numbers list, then it makes the addition
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        #Takes the lenght of the num_list
+        tam = len(num_list)
+        #Counter used in the while.
+        cont = 0
+        #Equals suma with 0.
+        suma = 0
+        #While tam been < than cont, keep adding and make cont+1
+        while cont < tam:
+            suma = suma + num_list[cont]
+            cont = cont + 1
+            #Return the result of the addition
+            return suma
+
+NUM_LIST = [1, 2, 3, 4, 5, 6]
--- primes.py
+++ primes.py
@@ -1,16 +1,62 @@
 """
-Your module documentation here
+This module verify if a number is prime or not.
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Contains the method is_prime
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Returns the value of the flag number TRUE or FALSEY
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        #The method was modified following the algorithm with do while.
+        #The function is evaluated after completing the condition.
+        #if not starts the elif or else.
+        if (num > 2):
+        #Declares the var for the condition.
+            primo = 0
+            divisor = 2
+            #Evaluation of the condition with module.
+            if ((num % divisor) == 0):
+                primo = primo + 1
+                divisor = divisor + 1
+            else:
+                divisor = divisor + 1
+                #Start the loop while it complements the do while
+                while(num < divisor):
+                    if ((num % divisor) == 0):
+                        primo = primo + 1
+                        divisor = divisor + 1
+                    else:
+                        divisor = divisor + 1
+            if (primo == 0):
+                flag = True
+            else:
+                flag = False
+        #If the condition isn't true compares and then return the flag.
+        elif(num == 2):
+            flag = True
+        else:
+            flag = False
+        #Imprime el numero con su bandera asignada.
+        return flag
+
+#Instanciar la clase. Se guarda en un objeto.
+numprim = prime_class()
+#Declaracion de variables.
+#Lista donde los primeros 4 numeros son primos y los otros no lo son.
+lnum = [2, 3, 5, 7, 1, 4, 6, 8]
+cont = 0
+tam = len(lnum)
+#While que servira de recorrido en la lista de numeros.
+while (cont < tam):
+    num = lnum[cont]
+    numprim.is_prime(num)
+    cont = cont + 1
+
+
+
